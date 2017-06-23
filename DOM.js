b94dfb56537308f6287df349a0470d3a622c84ec
ciaran1313 <caerfinn@gmail.com>
2017-06-23 18:25:23 -0400

    switched $$ for \\[

--- DOM.js
+++ DOM.js
@@ -14,33 +14,33 @@ function writeSteps(){
   const coefficientVariables = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'].slice(0, n+1).reverse();
 
   //writes a template polynomial to degree n
-  steps.innerHTML += "$$" + writeLaTexPolynomial(coefficientVariables, 'x', "f(x)") + "$$";
+  steps.innerHTML += "\\[" + writeLaTexPolynomial(coefficientVariables, 'x', "f(x)") + "\\]";
 
   //writes the finite differences equation
-  steps.innerHTML += "$$\\Delta^{n}y=an!(\\Delta{x})^{n}$$";
+  steps.innerHTML += "\\[\\Delta^{n}y=an!(\\Delta{x})^{n}\\]";
 
   //substitues values
-  steps.innerHTML += "$$" + delny + "= a \\cdot " + n + "!" + "\\cdot " + problem.delta_x + "^{" + n + "}$$";
+  steps.innerHTML += "\\[" + delny + "= a \\cdot " + n + "!" + "\\cdot " + problem.delta_x + "^{" + n + "}\\]";
 
   //evaluates power and factorial
-  steps.innerHTML += "$$" + delny + "= a \\cdot " + factorial(n) + "\\cdot " + Math.pow(problem.delta_x, n) + "$$";
+  steps.innerHTML += "\\[" + delny + "= a \\cdot " + factorial(n) + "\\cdot " + Math.pow(problem.delta_x, n) + "\\]";
 
   //evaluates multiplication
-  steps.innerHTML += "$$" + delny + "= a \\cdot " + (factorial(n) * Math.pow(problem.delta_x, n)) + "$$";
+  steps.innerHTML += "\\[" + delny + "= a \\cdot " + (factorial(n) * Math.pow(problem.delta_x, n)) + "\\]";
 
   //algebra
-  steps.innerHTML += "$$ a = \\frac{"+delny+"}{"+(factorial(n) * Math.pow(problem.delta_x, n))+"}$$";
+  steps.innerHTML += "\\[ a = \\frac{"+delny+"}{"+(factorial(n) * Math.pow(problem.delta_x, n))+"}\\]";
 
   //evaluates a
   const a = delny/(factorial(n) * Math.pow(problem.delta_x, n));
-  steps.innerHTML += "$$a = " + a + "$$"
+  steps.innerHTML += "\\[a = " + a + "\\]"
 
   //f(0)
   const f0 = problem.y_values[problem.x_values.indexOf(0)];
-  steps.innerHTML += "$$" + coefficientVariables[0] + "=f(0) = " + f0 + "$$";
+  steps.innerHTML += "\\[" + coefficientVariables[0] + "=f(0) = " + f0 + "\\]";
 
   //writes polynomial with a and f(0) included
-  steps.innerHTML += "$$" +  writeLaTexPolynomial([f0].concat(coefficientVariables.slice(1, coefficientVariables.length-1).concat(a)), 'x', "f(x)") + "$$";
+  steps.innerHTML += "\\[" +  writeLaTexPolynomial([f0].concat(coefficientVariables.slice(1, coefficientVariables.length-1).concat(a)), 'x', "f(x)") + "\\]";
 
   //////////////
   // for loop //
@@ -51,35 +51,35 @@ function writeSteps(){
   var y = problem.y_values[problem.x_values.indexOf(0)+1];
 
   //writes polynomial with x and y values
-  steps.innerHTML += "$$" +  writeLaTexPolynomial([f0].concat(coefficientVariables.slice(1, coefficientVariables.length-1).concat(a)), '(' + x + ')', y) + "$$";
+  steps.innerHTML += "\\[" +  writeLaTexPolynomial([f0].concat(coefficientVariables.slice(1, coefficientVariables.length-1).concat(a)), '(' + x + ')', y) + "\\]";
 
   //evaluates powers
   var firstTermValue = a*Math.pow(x,n);
-  steps.innerHTML += "$$" + y + "=" + firstTermValue;
+  steps.innerHTML += "\\[" + y + "=" + firstTermValue;
   for(i = coefficientVariables.length-2; i >= 1; i--) steps.innerHTML += '+' + Math.pow(x,i) + coefficientVariables[i];
   if (f0 > 0) steps.innerHTML += '+' + f0;
   else if (f0 < 0) steps.innerHTML += '-' + Math.abs(f0);
-  steps.innerHTML += "$$";
+  steps.innerHTML += "\\]";
 
   //algebra
-  steps.innerHTML += "$$" + y + (firstTermValue > 0? '-':'+') + Math.abs(firstTermValue) + (f0 > 0? '-':'+') + Math.abs(f0);
+  steps.innerHTML += "\\[" + y + (firstTermValue > 0? '-':'+') + Math.abs(firstTermValue) + (f0 > 0? '-':'+') + Math.abs(f0);
   for (i = coefficientVariables.length-2; i > 1; i--) steps.innerHTML += '-' + Math.pow(x,i) + coefficientVariables[i];
   steps.innerHTML += '=' + Math.pow(x,1) + coefficientVariables[1];
-  steps.innerHTML += "$$";
+  steps.innerHTML += "\\]";
 
   //evaluates addition and writes fraction
-  steps.innerHTML += "$$" + coefficientVariables[1] + '='
+  steps.innerHTML += "\\[" + coefficientVariables[1] + '='
   steps.innerHTML += "\\frac{"
   steps.innerHTML += String(y - firstTermValue - f0);
   for (i = coefficientVariables.length-2; i > 1; i--) steps.innerHTML += '-' + Math.pow(x,i) + coefficientVariables[i];
   steps.innerHTML += "}{" + Math.pow(x,1) + "}";
-  steps.innerHTML += "$$";
+  steps.innerHTML += "\\]";
 
   //evaluates fraction
-  steps.innerHTML += "$$" + coefficientVariables[1] + '='
+  steps.innerHTML += "\\[" + coefficientVariables[1] + '='
   steps.innerHTML += String((y - firstTermValue - f0)/Math.pow(x,1));
   for (i = coefficientVariables.length-2; i > 1; i--) steps.innerHTML += '-' + (Math.pow(x,i)/Math.pow(x,1)) + coefficientVariables[i];
-  steps.innerHTML += "$$";
+  steps.innerHTML += "\\]";
 
   //typesets
   MathJax.Hub.Typeset();
@@ -97,7 +97,7 @@ function displayNewProblem(){
 
   //writes hidden equation
   document.getElementById("equation").style = "display:none;";
-  document.getElementById("equation").innerHTML = "$$" + problem.equation.toLaTex() + "$$";
+  document.getElementById("equation").innerHTML = "\\[" + problem.equation.toLaTex() + "\\]";
   MathJax.Hub.Typeset();
 
   //draws header
